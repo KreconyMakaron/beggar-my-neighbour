cf3aa5a24021437b7aefc3a946f613db15ddd081
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-03-19 20:23:32 +0100

    meow

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	inputs = {
+  inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -7,26 +7,26 @@
     (flake-utils.lib.eachDefaultSystem
       (system:
         let 
-					pkgs = import nixpkgs { 
-            inherit system; 
-            config.allowUnfree = true;
-          };
-        in
-        {
-          packages = rec {
-            compile = pkgs.writeShellScriptBin "compile" ''
-                NIX_ENFORCE_NO_NATIVE=0 g++ display.cpp beg.cpp -o beg -O3 -std=c++23 -march=native -ffast-math -fopenmp
-            '';
-            default = compile;
-          };
+        pkgs = import nixpkgs { 
+        inherit system; 
+        config.allowUnfree = true;
+      };
+      in
+      {
+        packages = rec {
+          compile = pkgs.writeShellScriptBin "compile" ''
+            NIX_ENFORCE_NO_NATIVE=0 g++ display.cpp beg.cpp -o beg -O3 -std=c++23 -march=native -ffast-math -fopenmp
+          '';
+          default = compile;
+        };
 
-					devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              gcc
-              gdb
-              hyperfine
-						];
-					};
-				})
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            gdb
+            hyperfine
+          ];
+        };
+      })
     );
 }
